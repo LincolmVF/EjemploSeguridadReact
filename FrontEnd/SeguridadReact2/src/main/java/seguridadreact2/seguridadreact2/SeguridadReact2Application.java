622875f865a217d1f3375665f4895a11ab0629fd
Lincolm Villanueva <lincolmvf@gmail.com>
2025-05-07 11:24:57 -0500

    Primer commit

---
+++ FrontEnd/SeguridadReact2/src/main/java/seguridadreact2/seguridadreact2/SeguridadReact2Application.java
@@ -0,0 +1,13 @@
+package seguridadreact2.seguridadreact2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadReact2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeguridadReact2Application.class, args);
+    }
+
+}
---
+++ FrontEnd/SeguridadReact2/src/test/java/seguridadreact2/seguridadreact2/SeguridadReact2ApplicationTests.java
@@ -0,0 +1,13 @@
+package seguridadreact2.seguridadreact2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadReact2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
